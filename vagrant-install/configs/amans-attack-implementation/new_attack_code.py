ad42f8dba4bafe80922ea8b10a8c89f215f995cd
Priyansh Singh <priyanshsingh@outlook.com>
2024-01-25 03:02:54 +0530

    Revert "repo reset"
    This reverts commit 8539896088b35c84012f9efbb4fec5bc387edc18.

--- vagrant-install/configs/amans-attack-implementation/new_attack_code.py
+++
@@ -1,6 +0,0 @@
-from pcapng import FileScanner
-with open(r'attack-capture.pcapng', 'rb') as fp:
-    scanner = FileScanner(fp)
-    for block in scanner:
-        print(block)
-        # print(block._raw) #byte type raw data
