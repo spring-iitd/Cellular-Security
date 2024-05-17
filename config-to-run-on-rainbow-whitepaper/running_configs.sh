0abd55eb1cd6f403fc7a52f842af9be2e98ff448
priyanshs <priyanshsingh@outlook.com>
2024-05-17 18:46:57 +0530

    config-to-run-on-rainbow-whitepaper

---
+++ config-to-run-on-rainbow-whitepaper/running_configs.sh
@@ -0,0 +1,17 @@
+open5gs-nrfd -c nrf.yaml &
+open5gs-scpd -c scp.yaml &
+#open5gs-mmed -c mme.yaml &
+open5gs-ausfd -c ausf.yaml &
+open5gs-bsfd -c bsf.yaml &
+#open5gs-hssd -c hss.yaml &
+open5gs-nssfd -c nssf.yaml &
+open5gs-pcfd -c pcf.yaml &
+#open5gs-pcrfd -c pcrf.yaml &
+open5gs-amfd -c amf.yaml &
+#open5gs-sgwcd -c sgwc.yaml &
+#open5gs-sgwud -c sgwu.yaml &
+open5gs-udmd -c udm.yaml &
+open5gs-udrd -c udr.yaml &
+open5gs-upfd -c upf.yaml &
+open5gs-smfd -c smf.yaml &
+
