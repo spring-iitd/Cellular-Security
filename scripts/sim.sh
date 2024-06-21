1f657aeb0001a2bf2b3bf0c86c904737b3071ad4
Priyansh Singh <priyanshsingh@outlook.com>
2024-06-21 19:48:50 +0530

    changed vagrantfile to make network private-dhcp, offloaded trigger to sim.sh

---
+++ scripts/sim.sh
@@ -0,0 +1,5 @@
+sudo sh /home/vagrant/open5gs/misc/netconf.sh
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -w net.ipv6.conf.all.forwarding=1
+sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
+sudo ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUERADE
