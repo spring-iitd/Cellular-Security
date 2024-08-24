8dcfe37a70664f02e8ae3f91740145c4b00078f5
Prateek Bhaisora <prateek.bhaisora07@gmail.com>
2024-08-24 10:16:40 +0530

    added check_status run_all_srv stop_all_srv

---
+++ scripts/check_status.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+nfs=("open5gs-amfd" "open5gs-ausfd" "open5gs-bsfd" "open5gs-nrfd" "open5gs-nssfd" "open5gs-pcfd" "open5gs-scpd" "open5gs-smfd" "open5gs-upfd" "open5gs-udmd" "open5gs-udrd")
+
+running_count=0
+
+GREEN="\e[32m"
+RED="\e[31m"
+NC="\e[0m"
+
+echo "Status of Open5GS daemons..."
+echo ""
+
+for nf in "${nfs[@]}"; do
+    if ps -e | grep -q "$nf"; then
+        echo -e "${nf#open5gs-}: ${GREEN}running${NC}"
+        ((running_count++))
+    else
+        echo -e "${nf#open5gs-}: ${RED}not running${NC}"
+    fi
+done
+
+echo ""
+echo -e "$running_count NFs are running!"
